8ca52c65ec519e5bc30bad232f3ec0aa6f0f9879
Akansha Aggarwal <akanshaaggarwal9876@gmail.com>
2023-04-28 18:33:04 +0530

    final project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-// require('express-async-errors');
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
-// app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/products', productsRouter);
 
 // products route
 
